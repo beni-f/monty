d5ccffa9a763e600dcdd4372a29ddb8c8386e5f0
beni-f <benifissha@gmail.com>
2023-09-23 04:39:07 +0300

    updated

--- stack_fun.c
+++ stack_fun.c
@@ -89,13 +89,11 @@ void swap(stack_t **stack, unsigned int line_number)
 }
 void add(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = *stack;
     if (*stack == NULL || (*stack)->next == NULL)
     {
         fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
         exit(EXIT_FAILURE);
     }
     (*stack)->next->n += (*stack)->n;
-    (*stack) = (*stack)->next;
-    free(temp);
+    pop(stack, line_number);
 }
